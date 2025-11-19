69e2d0c6c9d3f4a2c4fca089df57247c88912c04
standardloop <joshuaeli1234@gmail.com>
2025-11-19 11:05:37 +0800

    clean up repo and add test for pulling github release

---
+++ lab.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <standardloop/json.h>
+
+int main(void)
+{
+    // JSON *json = JSONFromFile("lab/test.json");
+    JSON *json = StringToJSON("[{\"guid\": \"\", \"about\": \"\"}]");
+    if (json == NULL)
+    {
+        return EXIT_FAILURE;
+    }
+    PrintJSON(json);
+    // FreeJSON(json);
+
+    char *json_string = JSONToString(json);
+    if (json_string == NULL)
+    {
+        return EXIT_FAILURE;
+    }
+    // printf("%s", json_string);
+    free(json_string);
+    return EXIT_SUCCESS;
+}
