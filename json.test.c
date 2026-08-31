9e91eceda6914d14640c8836a33170c89469fb6f
standardloop <joshuaeli1234@gmail.com>
2026-08-31 12:22:29 +0800

    simple tests

--- json.test.c
+++ json.test.c
@@ -13,7 +13,35 @@ static void testJSONInit()
     FreeJSON(test);
 }
 
-static void testStringToJSON() {}
+static void testStringToJSON()
+{
+    // test 1
+    char *test_string = "[]";
+    JSON *test_json = NULL;
+
+    test_json = StringToJSON("[]");
+    assert(test_json != NULL);
+    assert(test_json->root->value_type == JSONLIST_t);
+    FreeJSON(test_json);
+
+    // test 2
+    test_string = "{}";
+    test_json = NULL;
+
+    test_json = StringToJSON(test_string);
+    assert(test_json != NULL);
+    assert(test_json->root->value_type == JSONOBJ_t);
+    FreeJSON(test_json);
+
+    // test 3
+    test_string = "{\"foo\": \"bar\", \"bar\": \"foo\"}";
+    test_json = NULL;
+
+    test_json = StringToJSON(test_string);
+    assert(test_json != NULL);
+    assert(test_json->root->value_type == JSONOBJ_t);
+    FreeJSON(test_json);
+}
 
 extern void TestJSON()
 {
